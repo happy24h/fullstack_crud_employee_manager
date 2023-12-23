9ea2855fc47910905d13921e2c5bd5350b1f0ad9
@anhnv99 <vietanhhappy99@gmail.com>
2023-12-23 12:38:05 +0700

    create backend

---
+++ backend/src/employees/dto/create-employee.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty } from 'class-validator';
+// ID/Name/Date of Bird/Gender/Email/Address
+export class CreateEmployeeDto {
+  @IsNotEmpty({ message: 'Tên không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Ngày sinh không được để trống' })
+  date_of_bird: string;
+
+  @IsNotEmpty({ message: 'Giới tính không đươc để trống' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Địa chỉ không được để trống' })
+  address: string;
+}
